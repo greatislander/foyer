368b5a0fc0b95e489c543289acfcfaf04c5e0ac9
Ned Zimmerman <ned@bight.ca>
2017-05-04 15:36:52 -0300

    Update stages.

--- dist/scripts/foyer.js
+++ dist/scripts/foyer.js
@@ -70,11 +70,27 @@
 /* 0 */
 /***/ (function(module, exports) {
 
-var openDoor = document.getElementById("open");
+var doorFrame = document.getElementById('doorframe'),
+  animationToAjar = document.getElementById('ajar'),
+  animationToOpen = document.getElementById('open'),
+  animationToClosed = document.getElementById('ajar'),
+  button = document.getElementById('open-button');
 
-window.onload = function() {
-  openDoor.beginElement();
-}
+button.addEventListener('click', function() {
+  if (doorFrame.classList.contains('ajar')) {
+    doorFrame.classList.remove('ajar');
+    doorFrame.classList.add('open');
+    animationToOpen.beginElement();
+  } else if (doorFrame.classList.contains('open')) {
+    doorFrame.classList.remove('open');
+    doorFrame.classList.add('closed');
+    animationToClosed.beginElement();
+  } else if (doorFrame.classList.contains('closed')) {
+    doorFrame.classList.remove('closed');
+    doorFrame.classList.add('ajar');
+    animationToAjar.beginElement();
+  }
+}, false);
 
 
 /***/ }),
